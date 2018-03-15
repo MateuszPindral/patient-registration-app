ac4170a2d9ac3ab1550eeee25077f0b0d0795dce
SebastianJaroszek <sebastian_jaroszek@emstories.pl>
2018-03-15 20:52:07 +0100

    Changes in RegistrationController

--- src/main/java/pl/sda/patient_registration_app/web/RegistrationController.java
+++ src/main/java/pl/sda/patient_registration_app/web/RegistrationController.java
@@ -1,17 +1,29 @@
 package pl.sda.patient_registration_app.web;
 
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import pl.sda.patient_registration_app.bo.RegistrationFinder;
 
 @Controller
 public class RegistrationController {
+
+    private final RegistrationFinder registrationFinder;
+
+    @Autowired
+    public RegistrationController(RegistrationFinder registrationFinder) {
+        this.registrationFinder = registrationFinder;
+    }
 
     @GetMapping("/rejestracja")
     public ModelAndView showRegistrationPage() {
 
         ModelAndView mav = new ModelAndView("rejestracja");
+
+        mav.addObject("visits", registrationFinder.showAllVisits());
 
         return mav;
     }
