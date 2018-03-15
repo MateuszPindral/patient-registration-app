50e8fea203be1145b86b9f93ec345e945d01f1a6
SebastianJaroszek <sebastian_jaroszek@emstories.pl>
2018-03-15 20:46:32 +0100

    Implementation of find all of visits (RegistrationFinder).

--- src/main/java/pl/sda/patient_registration_app/bo/RegistrationFinder.java
+++ src/main/java/pl/sda/patient_registration_app/bo/RegistrationFinder.java
@@ -1,7 +1,17 @@
 package pl.sda.patient_registration_app.bo;
 
 import org.springframework.stereotype.Service;
+import pl.sda.patient_registration_app.dto.VisitDto;
+
+import java.util.List;
+
+import static pl.sda.patient_registration_app.repository.VisitsRepository.visits;
 
 @Service
 public class RegistrationFinder {
+
+    public List<VisitDto> showAllVisits(){
+        return visits;
+    }
+
 }
