889cab1e6c605b5bcf6d34b9578fb571384e9026
SebastianJaroszek <sebastian_jaroszek@emstories.pl>
2018-03-18 18:16:15 +0100

    Refactoring: - transfer methods from RegistrationFinder to VisitsFinder - separate mapping methods to UtilsService - transfer methods form RegistrationService to VisitsService.

--- src/main/java/pl/sda/patient_registration_app/bo/VisitsFinder.java
+++ src/main/java/pl/sda/patient_registration_app/bo/VisitsFinder.java
@@ -1,7 +1,31 @@
 package pl.sda.patient_registration_app.bo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.sda.patient_registration_app.dto.VisitDto;
+import pl.sda.patient_registration_app.repository.VisitsRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class VisitsFinder {
+
+    private VisitsRepository visitsRepository;
+    private UtilsService utilsService;
+
+    @Autowired
+    public VisitsFinder(VisitsRepository visitsRepository, UtilsService utilsService){
+        this.visitsRepository = visitsRepository;
+        this.utilsService = utilsService;
+    }
+
+    @Transactional
+    public List<VisitDto> showAllVisits() {
+        return visitsRepository.findAll().stream()
+                .map(v -> utilsService.mapVisitToVisitDto(v))
+                .collect(Collectors.toList());
+    }
+
 }
