983304a56ae22629f181d17fcfaecf00f4043dd1
BartGor <bartosz.gorny@hotmail.com>
2018-03-19 18:40:22 +0100

    add DoctorDayDto

--- src/main/java/pl/sda/patient_registration_app/dto/DoctorDayDto.java
+++ src/main/java/pl/sda/patient_registration_app/dto/DoctorDayDto.java
@@ -1,6 +1,25 @@
 package pl.sda.patient_registration_app.dto;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import pl.sda.patient_registration_app.bo.UtilsService;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DoctorDayDto {
+
+
+    private UtilsService utilsService;
+
+    private DoctorDto doctorDto;
+    private List<VisitDto> visits;
+    private final List<LocalTime> HOURS = utilsService.getHours();
+
+    @Autowired
+    public DoctorDayDto(UtilsService utilsService) {
+        this.utilsService = utilsService;
+    }
 
 
 }
