1da4d2c9054896d98502a393977a1e23ba18499b
MateuszPindral <pindralmateusz7@gmail.com>
2018-03-22 17:23:37 +0100

    Merge branches 'DevBartGor' and 'MainDevBranch' of https://github.com/MateuszPindral/patient-registration-app into MainDevBranch
    # Conflicts:
    #	src/main/webapp/resources/views/main.jsp

--- src/main/java/pl/sda/patient_registration_app/config/SecurityConfiguration.java
+++ src/main/java/pl/sda/patient_registration_app/config/SecurityConfiguration.java
@@ -3,9 +3,11 @@ package pl.sda.patient_registration_app.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -22,13 +24,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication().withUser("De").password("Bill").roles("ADMIN", "DBA");
     }
 
+/*    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/**");
+    }*/
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/resources/**").permitAll();
+
         http.authorizeRequests()
                 .antMatchers("/rejestracja").authenticated()
                 .antMatchers("/tabelaWizyt").authenticated()
                 .antMatchers("/rejestracja/specjalista").authenticated()
-                .antMatchers("/*").permitAll()
                 .and().formLogin()
                 .and().exceptionHandling().accessDeniedPage("/error")
                 .and().csrf().disable();
