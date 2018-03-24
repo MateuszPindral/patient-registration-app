adf370f448a3cba63378d6081516e6a310add02d
MateuszPindral <pindralmateusz7@gmail.com>
2018-03-24 14:59:21 +0100

    @Validated

--- src/main/java/pl/sda/patient_registration_app/dto/NewPatientRegistrationDto.java
+++ src/main/java/pl/sda/patient_registration_app/dto/NewPatientRegistrationDto.java
@@ -1,5 +1,6 @@
 package pl.sda.patient_registration_app.dto;
 
+import com.sun.istack.internal.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +13,19 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class NewPatientRegistrationDto {
+
+    @NotNull
     private String firstName;
+    @NotNull
     private String lastName;
+    @NotNull
     private String login;
+    @NotNull
     private String password;
+
+    private String matchingPassword;
+
+
+    @NotNull
     private String email;
 }
--- src/main/java/pl/sda/patient_registration_app/web/NewPatientRegistrationController.java
+++ src/main/java/pl/sda/patient_registration_app/web/NewPatientRegistrationController.java
@@ -1,6 +1,7 @@
 package pl.sda.patient_registration_app.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,7 +29,7 @@ public class NewPatientRegistrationController {
     }
 
     @PostMapping(value = "/nowyUzytkownik/zarejestruj")
-    public ModelAndView postNewUserPage(@ModelAttribute("newUser") NewPatientRegistrationDto newPatientRegistrationDto) {
+    public ModelAndView postNewUserPage(@ModelAttribute("newUser") @Validated NewPatientRegistrationDto newPatientRegistrationDto) {
 
         ModelAndView mav = new ModelAndView("rejestracjaWynik");
 
